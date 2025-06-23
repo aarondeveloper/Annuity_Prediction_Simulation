c176bb188692f1c5ca0754ef93511aecd5c34efa
Aaron Oster <aaron.o.developer@gmail.com>
2025-06-22 22:32:09 -0400

    added glue job and upload to s3 script

--- src/glue_redshift_job.py
+++ src/glue_redshift_job.py
@@ -10,11 +10,6 @@ from pyspark.sql import SparkSession
 # Get job parameters
 args = getResolvedOptions(sys.argv, [
     'JOB_NAME',
-    'REDSHIFT_HOST',
-    'REDSHIFT_PORT',
-    'REDSHIFT_DB',
-    'REDSHIFT_USER',
-    'REDSHIFT_PASSWORD',
     'S3_BUCKET_NAME',
     'REDSHIFT_IAM_ROLE_ARN'
 ])
@@ -30,14 +25,9 @@ def main():
     """Main ETL job to load annuity data into Redshift."""
     
     # Configuration from Glue Job Parameters
-    redshift_host = args['REDSHIFT_HOST']
-    redshift_port = args['REDSHIFT_PORT']
-    redshift_db = args['REDSHIFT_DB']
-    redshift_user = args['REDSHIFT_USER']
-    redshift_password = args['REDSHIFT_PASSWORD']
     s3_bucket = args['S3_BUCKET_NAME']
     iam_role_arn = args['REDSHIFT_IAM_ROLE_ARN']
-    
+    redshift_connection_name = "Redshift connection" # The name of the connection you created
     redshift_table = 'annuity_policies'
     
     # S3 paths
@@ -52,27 +42,22 @@ def main():
         df = spark.read.option("header", "true").option("inferSchema", "true").csv(s3_data_path)
         
         print(f"Successfully read {df.count()} records from CSV")
-        print("Data schema:")
-        df.printSchema()
         
         # Convert to DynamicFrame for Glue
         dynamic_frame = DynamicFrame.fromDF(df, glueContext, "annuity_data")
         
-        # Write to Redshift using Glue's native Redshift connector
-        print("Writing data to Redshift...")
+        # Write to Redshift using the pre-defined Glue Connection
+        print(f"Writing data to Redshift using connection: {redshift_connection_name}")
         
         glueContext.write_dynamic_frame.from_options(
             frame=dynamic_frame,
             connection_type="redshift",
             connection_options={
-                "url": f"jdbc:redshift://{redshift_host}:{redshift_port}/{redshift_db}",
+                "connectionName": redshift_connection_name,
                 "dbtable": redshift_table,
-                "user": redshift_user,
-                "password": redshift_password,
                 "redshiftTmpDir": s3_temp_dir,
                 "aws_iam_role": iam_role_arn,
                 "preactions": f"DROP TABLE IF EXISTS {redshift_table}; CREATE TABLE {redshift_table} (policy_id VARCHAR, issue_age INT, issue_date VARCHAR, premium BIGINT, has_gmwb BOOLEAN, has_gmib BOOLEAN, gmwb_rider_fee FLOAT, gmib_rider_fee FLOAT, death_benefit_type VARCHAR, death_benefit_fee FLOAT, has_beneficiary BOOLEAN, me_fees FLOAT, admin_fees FLOAT, rider_fees FLOAT, total_fees FLOAT);",
-                "postactions": "COMMIT"
             }
         )
         
